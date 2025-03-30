c469b546d8ab54274a85171f2e00b2418b2e14fc
Enricomaria Di Rosolini <enricomariadr99@gmail.com>
2025-03-30 22:36:52 +0200

    Adding other pages

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/studio')
+def thestudio():
+    return render_template('studio.html')
+
+@app.route('/contatti')
+def contatti():
+    return render_template('contacts.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
